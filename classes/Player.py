c2e3adf20343f6545731cb535951f369045ac0d2
Maxi25554 <maxin25554@gmail.com>
2021-10-03 01:41:52 +1000

    player

---
+++ classes/Player.py
@@ -0,0 +1,10 @@
+import pygame
+
+class Player:
+    def __init__(self, x, y):
+        player = self.load_image('images/player.png')
+        self.set_image(player, 25, 32)
+        
+        self.gravity = 0
+
+        self.handle_key_events = True
--- main.py
+++ main.py
@@ -5,26 +5,17 @@ from pygame.locals import *
 
 pygame.init()
 
-# Initial variables
 size = width, height = 800, 600
-speed = [2, 2]
-black = 0, 0, 0
 fps = 30
 pygame.mouse.set_visible(0)
-
-
-
-# Background
+pygame.display.set_caption('decay hook')
 bg = pygame.image.load("images/wood_bg.jpg")
 
-# Creates the screen
+# Creates the screen/window
 screen = pygame.display.set_mode(size)
 
-pygame.display.set_caption('decay hook')
-
 # Main loop
-x = 1
 while True:
+
     screen.blit(bg, [0, 0])
-    
     pygame.display.flip()
