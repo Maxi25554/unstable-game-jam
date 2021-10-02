0e4e3326ceb8366c10004c143f3d8e49f8c4bbed
David <davidmakesweirdtutorials@gmail.com>
2021-10-02 11:40:49 -0400

    set image added to gameobject class

--- classes/GameObject.py
+++ classes/GameObject.py
@@ -13,6 +13,14 @@ class GameObject:
         self.gravity = 0
         self.angle = 0
 
+    def set_image(self, image, width, height):
+        self.image_orig = pygame.image.load(image).convert_alpha()
+        self.image_orig = pygame.transform.scale(self.image_orig, (width, height))
+        self.width = width
+        self.height = height
+        self.image = self.image_orig.copy()
+        self.rect = pygame.Rect(self.x, self.y, width, height)
+
     def update(self):
         self.y_speed = self.y_speed + self.gravity
         self.x += self.x_speed
