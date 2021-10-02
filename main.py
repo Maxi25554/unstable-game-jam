bb9d22ed7b587d0091e0e97ea4eac0e0df469907
Maxi25554 <maxin25554@gmail.com>
2021-10-03 02:16:52 +1000

    yes

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ pygame.init()
 
 size = width, height = 800, 600
 fps = 30
-pygame.mouse.set_visible(0)
 pygame.display.set_caption('decay hook')
 bg = pygame.image.load("images/wood_bg.jpg")
 
@@ -17,13 +16,8 @@ screen = pygame.display.set_mode(size)
 
 # Main loop
 while True:
-    ev = pygame.event.get()
-    for event in ev:
-
-        # handle click
-        if event.type == pygame.MOUSEBUTTONUP:
-            pos = pygame.mouse.get_pos()
-            print (pos)
+    for event in pygame.event.get():
+        
     
     screen.blit(bg, [0, 0])
     pygame.display.flip()
