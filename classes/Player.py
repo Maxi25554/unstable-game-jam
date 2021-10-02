0d997234037f7ed9a87a0c7c159ced041edc0c08
David <davidmakesweirdtutorials@gmail.com>
2021-10-02 11:47:48 -0400

    extended from gameobject

--- classes/Player.py
+++ classes/Player.py
@@ -1,6 +1,7 @@
 import pygame
+import GameObject
 
-class Player:
+class Player(GameObject):
     def __init__(self, x, y):
         player = self.load_image('images/player.png')
         self.set_image(player, 25, 32)
