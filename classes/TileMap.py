c09d2e7530435e7bec18775b519b22b97ba168c4
David <davidmakesweirdtutorials@gmail.com>
2021-10-02 13:50:16 -0400

    added a few other methods

--- classes/TileMap.py
+++ classes/TileMap.py
@@ -36,7 +36,6 @@ class TileMap:
                 self.tilemap[i] = list
 
     def add_tile(self, x_pos, y_pos, tile):
-
         #Check if tile variable is a valid tile
         if tile > self.highest_tile_index or tile < self.lowest_tile_index:
             print(f"Error: Tile cannot be bigger than index {self.highest_tile_index} or lower than index {self.lowest_tile_index}")
@@ -54,3 +53,23 @@ class TileMap:
                 pos_y = y
         
         self.tilemap[pos_y][pos_x] = tile
+
+    def remove_tile(self, x_pos, y_pos):
+        #Find specified tile position and change it accordingly
+        pos_x = 0
+        pos_y = 0
+        for x in range(self.mapwidth):
+            if x == x_pos:
+                pos_x = x
+        
+        for y in range(self.mapheight):
+            if y == y_pos:
+                pos_y = y
+        
+        self.tilemap[pos_y][pos_x] = 0 #Change tile back to air(essentially destroying whatever was there)
+
+    def display_tile_map(self):
+        pass
+
+    def check_collisions(self):
+        pass
